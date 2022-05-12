014b7b811cb01466f60c0167c0c6ade8af7e811a
Javier Tovar <javieralexander31@hotmail.com>
2022-05-12 01:26:39 -0400

    servicios

--- src/js/main.js
+++ src/js/main.js
@@ -16,3 +16,19 @@ window.onscroll = () =>{
         document.querySelector('.header .navbar').classList.remove('active');
     }
 }
+
+
+// slider
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+},
+    pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+},
+
+});
