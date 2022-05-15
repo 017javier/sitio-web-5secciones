a7477bb6cbaaba189d3bdaf4ccf08b5b5dda8bfe
Javier Tovar <javieralexander31@hotmail.com>
2022-05-14 22:07:12 -0400

    alcance

--- src/js/main.js
+++ src/js/main.js
@@ -32,3 +32,31 @@ var swiper = new Swiper(".mySwiper", {
 },
 
 });
+// slider alcance
+var swiperAlcance = new Swiper(".alcance__contenedor", {
+    spaceBetween: 24,
+    loop: true,
+    grabCursor: true,
+
+    pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    },
+    breakpoints: {
+        1024: {
+            slidesPerView: 2,
+            spaceBetween: 48,
+        },
+        768: {
+            slidesPerView: 1,
+            spaceBetween: 38,
+        },
+        576: {
+            slidesPerView: 1
+            
+        },
+        
+        
+    },
+
+});
